95cd0d55cf06a851b4ee7b08e2fe47c6c742cb00
mararo-ops <danielmararo1@gmail.com>
2023-11-27 22:06:01 +0300

    complete assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+# lib/oxford_comma.py
+
+def oxford_comma(array):
+    if len(array) == 1:
+        return array[0]
+    elif len(array) == 2:
+        return f"{array[0]} and {array[1]}"
+    else:
+        comma_separated = ", ".join(array[:-1])
+        return f"{comma_separated}, and {array[-1]}"
